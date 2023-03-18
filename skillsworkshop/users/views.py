e33f675b967bc028b9abe5a9f0299393d9357937
anneewhere <114482462+anneewhere@users.noreply.github.com>
2023-03-18 12:05:34 +0800

    Up-To-Date on Set up
    Set up of users and workshops. Main branch organised.

--- skillsworkshop/users/views.py
+++ skillsworkshop/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . models import Users
+from .serializers import UsersSerializer
 
 # Create your views here.
--- skillsworkshop/workshops/models.py
+++ skillsworkshop/workshops/models.py
@@ -1,4 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth import get_user_model
 # Create your models here.
