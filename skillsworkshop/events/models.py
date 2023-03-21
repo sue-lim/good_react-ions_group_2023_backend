ad6f97902e55322ed29448d90d48db7e54a27c23
anneewhere <114482462+anneewhere@users.noreply.github.com>
2023-03-21 18:08:02 +0800

    EVENTS MODELS.PY
    updated and merge

--- skillsworkshop/events/models.py
+++ skillsworkshop/events/models.py
@@ -1,23 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
 
-# User = get_user_model() - to unhash when merged with user (TUWM)
+User = get_user_model()
+
 
 class Event(models.model):
     event_title = models.CharField(max_length=200)
     description = models.Textfield()
-    datetime = #find on google
+    datetime = models.DateTimeField()
     location = models.CharField(max_length=200)
-    max_participants = models.CharField(max_length=200) #should be integer
+    max_participants = models.CharField(max_length=200)  # should be integer
     image = models.URLField()
     is_open = models.BooleanField()
-    # organiser = models.ForeignKey(
-    #     User,
-    #     on_delete=models.CASCADE,
-    #     related_name='organiser'
-    # ) UHWC
-    # attendees = models.ManyToManyField(
-    #     User,
-    #     related_name='attendee'
-    # )
+    organiser = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='organiser'
+    )
+    attendees = models.OneToManyField(
+        User,
+        related_name='attendee',
+        on_delete=models.CASCADE,
+    )
+
+
+@property
+def max_capacity(self):
+    # how to add max participants
+    return self.max_participants.aggregate(sum=models.sum('max_participant'))['sum']
