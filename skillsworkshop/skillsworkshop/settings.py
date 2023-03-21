99ecb99690bb58464c3d322bc689c1026b495799
SushiNspice <113986306+sue-lim@users.noreply.github.com>
2023-03-21 18:12:38 +0800

    Users Models UPdated

--- skillsworkshop/skillsworkshop/settings.py
+++ skillsworkshop/skillsworkshop/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    # 'users.apps.UsersConfig',
 ]
 
 REST_FRAMEWORK = {
@@ -50,9 +50,9 @@ REST_FRAMEWORK = {
     ]
 }
 
-AUTH_USER_MODEL = [
-'users.CustomUser',
-]
+# AUTH_USER_MODEL = [
+# 'users.CustomUser',
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- skillsworkshop/users/models.py
+++ skillsworkshop/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+User = AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=100)
@@ -12,8 +13,9 @@ class CustomUser(AbstractUser):
     location = models.CharField(max_length=200)
     profile_picture = models.URLField()
     is_mentor = models.BooleanField()
-    is_mentee = models.BooleanField()
+    is_mentee = models.BooleanField(default=True)
     is_private = models.BooleanField()
+    
     
     pass
 
