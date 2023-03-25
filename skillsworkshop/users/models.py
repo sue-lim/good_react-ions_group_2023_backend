2010616200447bce4607ca9533c1cc568003251e
Ijeona <Ijeona@users.noreply.github.com>
2023-03-25 12:18:51 +0800

    Changed User Model to create Superuser

--- skillsworkshop/users/models.py
+++ skillsworkshop/users/models.py
@@ -6,16 +6,16 @@ User = AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
-    bio = models.TextField()
-    phone_number = models.IntegerField()
+    bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20,blank=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     email = models.CharField(max_length=30, blank=True)
-    location = models.CharField(max_length=200)
-    profile_picture = models.URLField()
-    is_mentor = models.BooleanField()
+    location = models.CharField(max_length=200, blank=True)
+    profile_picture = models.URLField(blank=True)
+    is_mentor = models.BooleanField(default=False)
     is_mentee = models.BooleanField(default=True)
-    is_private = models.BooleanField()
+    is_private = models.BooleanField(default=False)
     
     
     pass
