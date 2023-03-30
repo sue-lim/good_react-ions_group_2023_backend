6e8d1179160d6bd232afd11f19b65d56f19c1734
anneewhere <114482462+anneewhere@users.noreply.github.com>
2023-03-30 21:35:40 +0800

    Update of sponsor on models.py in events

--- skillsworkshop/events/models.py
+++ skillsworkshop/events/models.py
@@ -4,16 +4,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Topic(models.Model):
-    
-    name = models.CharField (max_length=200)
-        
+
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
-    
+
+
 class Event(models.Model):
-    topic = models.ForeignKey (
-        Topic, 
+    topic = models.ForeignKey(
+        Topic,
         on_delete=models.CASCADE,
         related_name='events',
 
@@ -25,6 +27,7 @@ class Event(models.Model):
     max_participants = models.CharField(max_length=200)  # should be integer
     image = models.URLField()
     is_open = models.BooleanField()
+    sponser = models.CharField(max_length=100, blank=True)
     organizer = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -36,9 +39,9 @@ class Event(models.Model):
         related_name='attendees',
         # blank=True,
     )
+
     def __str__(self):
         return self.event_title
-
 
 
 @property
